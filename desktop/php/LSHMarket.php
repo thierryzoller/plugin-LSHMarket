6a7cdef0a41d3e4803891de7a06cfa9799376580
Zoller Thierry <thierry@zoller.lu>
2019-08-19 18:06:58 +0200

    l
    k

--- desktop/php/LSHMarket.php
+++ desktop/php/LSHMarket.php
@@ -37,7 +37,7 @@ if ($sourcesListRaw !== null) {
         }
     }
 
-    \usort($sourcesList, array('LSHMarket', 'cmpByOrder'));
+    \usort($sourcesList, array('LSHMarketforJ', 'cmpByOrder'));
 }
 
 sendVarToJs('sourcesList', $sourcesList);
